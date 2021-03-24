8693cb28a884f2a8ff44a4d0ac972826c3c2aba2
Derlis <dsgezika@gmail.com>
2021-03-24 00:02:46 -0300

    97% finalizado

---
+++ senai_spmedgroup_03_DQL.sql.sql
@@ -0,0 +1,17 @@
+-- DQL
+
+SELECT * FROM	Clinicas;
+
+SELECT * FROM Consultas;
+
+SELECT * FROM Especialidades;
+
+SELECT * FROM Medicos;
+
+SELECT * FROM Pacientes;
+
+SELECT * FROM Situacao;
+
+SELECT * FROM tipoUsuarios;
+
+SELECT * FROM Usuarios;
